41f6da62dc80ea9823bae4867461aefb93b44f62
Jeffrey <jeffreymartespolanco@hotmail.com>
2019-12-10 17:15:50 -0800

    everything showing on the screen

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import {numbers} from "../../../data";
-import NumberButton from "../NumberButtons/NumberButton"
+import { numbers } from "../../../data";
+import NumberButton from "../NumberButtons/NumberButton";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders.
@@ -12,16 +12,13 @@ import { numbers } from '../../../data'
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [nums, setNumbers] = useState(numbers);
-  let holder = nums.map(item=> item);
-  console.log(holder)
-  
-  return(
+  return (
     <div>
-     <NumberButton oneToZero = {holder}/>
-     </div>
-  )
-
-   
+      {nums.map(item => (
+        <NumberButton oneToZero={item} />
+      ))}
+    </div>
+  );
 };
 /* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../App.css"
 const Display = props => {
-  return <div className="display">{props.Display}</div>;
+  return <div onClick= {props.show}className="display"></div>;
 };
 /* Display any props data here */
 export default Display
